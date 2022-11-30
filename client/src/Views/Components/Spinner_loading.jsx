a6ba25a5c9bc58ac5b2e89a405a0985e5003f604
omarurregodev <omarurrego07@gmail.com>
2022-11-29 23:20:49 -0500

    modificacion visual

--- client/src/Views/Components/Spinner_loading.jsx
+++ client/src/Views/Components/Spinner_loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { MDBSpinner } from 'mdb-react-ui-kit';
+import { MDBSpinner, MDBTypography } from 'mdb-react-ui-kit';
 
 export default function SpinnerLoading() {
   return (
     <>
+        <MDBTypography variant='h3'>Iniciando...</MDBTypography>
         <MDBSpinner role='status' className='mx-2' color='info'>
             <span className='visually-hidden'>Loading...</span>
         </MDBSpinner>
--- client/src/Views/Logout.jsx
+++ client/src/Views/Logout.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { MDBContainer, MDBSpinner, MDBTypography } from 'mdb-react-ui-kit';
 
 const Logout = () => {
   const navigate = useNavigate();
@@ -9,7 +10,17 @@ const Logout = () => {
     }, 2000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div>Adios</div>;
+  return (
+      <>
+      <MDBContainer className="p-0 d-flex align-items-center justify-content-center" style={{height:"100vh"}} fluid>
+        <MDBTypography variant='h3'>Cerrando sesión...</MDBTypography>
+        <MDBSpinner role='status' className='mx-1' color='danger'>
+            <span className='visually-hidden'>  Loading...</span>
+        </MDBSpinner>
+      </MDBContainer>
+      </>
+
+  )
 };
 
 export default Logout;
